e64cfb7ae1f579c8589d24d883b905ef02495262
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-07-29 16:05:53 -0500

    [add] error handling to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,32 +2,47 @@ import bookService from '../services/books'
 
 export async function fetchBooks (dispatch) {
   dispatch({ type: 'START_LOADER' })
-  const books = await bookService.getAll()
-  dispatch({
-    type: 'SET_BOOKS_COLLECTION',
-    books
-  })
-  dispatch({ type: 'STOP_LOADER' })
+  try {
+    const books = await bookService.getAll()
+    dispatch({
+      type: 'SET_BOOKS_COLLECTION',
+      books
+    })
+  } catch (error) {
+    console.log(error)
+  } finally {
+    dispatch({ type: 'STOP_LOADER' })
+  }
 }
 
 export async function createBook (dispatch, book) {
   dispatch({ type: 'START_LOADER' })
-  const newBook = await bookService.create(book)
-  dispatch({
-    type: 'CREATE_BOOK',
-    book: newBook
-  })
-  dispatch({ type: 'STOP_LOADER' })
+  try {
+    const newBook = await bookService.create(book)
+    dispatch({
+      type: 'CREATE_BOOK',
+      book: newBook
+    })
+  } catch (error) {
+    console.log(error)
+  } finally {
+    dispatch({ type: 'STOP_LOADER' })
+  }
 }
 
 export async function removeBook (dispatch, book) {
   dispatch({ type: 'START_LOADER' })
-  const removeBook = await bookService.delete(book.id)
-  dispatch({
-    type: 'REMOVE_BOOK',
-    book
-  })
-  dispatch({ type: 'STOP_LOADER' })
+  try {
+    await bookService.delete(book.id)
+    dispatch({
+      type: 'REMOVE_BOOK',
+      book
+    })
+  } catch (error) {
+    console.log(error)
+  } finally {
+    dispatch({ type: 'STOP_LOADER' })
+  }
 }
 
 export function changeBooksFilter (filter) {
