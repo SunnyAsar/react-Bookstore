c89c17d4bdf8047b394621d68375eb7598efc9da
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-07-30 09:22:59 -0500

    [add] redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
 import rootReducer from './reducers'
 import './index.css'
 
-const store = createStore(rootReducer)
-ReactDOM.render(<Provider store={store} ><App /></Provider>, document.getElementById('root'))
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
