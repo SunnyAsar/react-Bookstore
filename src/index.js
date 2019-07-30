04fd9bd2f10ab3b0dfa27bede0fc3dcb675fa477
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-07-30 15:31:33 -0500

    [add] redux devtools

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import App from './components/App'
-import { Provider } from 'react-redux'
 import rootReducer from './reducers'
 import './index.css'
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store} >
