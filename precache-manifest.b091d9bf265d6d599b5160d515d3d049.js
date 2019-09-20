336e39a83e66f995679ce8ccbd1129a2c81a4aa3
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-09-20 09:48:21 -0500

    Updates

---
+++ precache-manifest.b091d9bf265d6d599b5160d515d3d049.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8cd4b12eb18cb6c2b119ce89635b65",
+    "url": "/react-Bookstore/index.html"
+  },
+  {
+    "revision": "cc6f74c3d3766e03f24e",
+    "url": "/react-Bookstore/static/css/main.ecadd82a.chunk.css"
+  },
+  {
+    "revision": "8e31290e8b8f5fb13a2a",
+    "url": "/react-Bookstore/static/js/2.8fa76f34.chunk.js"
+  },
+  {
+    "revision": "cc6f74c3d3766e03f24e",
+    "url": "/react-Bookstore/static/js/main.869ffaf6.chunk.js"
+  },
+  {
+    "revision": "a27aa7963b38fc9b3a05",
+    "url": "/react-Bookstore/static/js/runtime~main.4472750d.js"
+  }
+]);
